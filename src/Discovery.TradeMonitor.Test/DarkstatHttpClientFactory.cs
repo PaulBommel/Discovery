72920cf3d0c885a3b9718c6ff6e604a00421b927
Paul Bommel <Paul.Bommel1234@arcor.de>
2025-01-29 09:52:45 +0100

    Added testproject for Discovery.TradeMonitor

---
+++ src/Discovery.TradeMonitor.Test/DarkstatHttpClientFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discovery.TradeMonitor.Test
+{
+    internal class DarkstatHttpClientFactory : IHttpClientFactory
+    {
+        public HttpClient CreateClient(string name)
+         => new()
+         {
+             BaseAddress = new("https://darkstat.dd84ai.com"),
+             DefaultRequestHeaders = {
+                 { HttpRequestHeader.Accept.ToString(), [ MediaTypeNames.Application.Json ] }
+             }
+         };
+    }
+    internal class DarkstatStagingHttpClientFactory : IHttpClientFactory
+    {
+        public HttpClient CreateClient(string name)
+         => new()
+         {
+             BaseAddress = new("https://darkstat-staging.dd84ai.com"),
+             DefaultRequestHeaders = {
+                 { HttpRequestHeader.Accept.ToString(), [ MediaTypeNames.Application.Json ] }
+             }
+         };
+    }
+}
