12955b35ad8958085d5fb5b7a548df9198eccaab
Paul Bommel <Paul.Bommel1234@arcor.de>
2025-03-07 10:51:37 +0100

    Started basic ShipOverview

---
+++ src/Discovery.Prototypes.TradeMonitor/Model/ShipOverviewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discovery.Prototypes.TradeMonitor.Model
+{
+    using Discovery.TradeMonitor;
+    public class ShipOverviewModel(ShipInfo ship, int routes)
+    {
+        public ShipInfo Ship { get; } = ship;
+        public int Routes { get; } = routes;
+    }
+}
---
+++ src/Discovery.Prototypes.TradeMonitor/ViewModels/ShipOverviewViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Discovery.Prototypes.TradeMonitor.ViewModels
+{
+    using Model;
+    internal sealed class ShipOverviewViewModel
+    {
+        #region Members
+
+        #endregion
+
+        #region Constructors
+
+        public ShipOverviewViewModel()
+        {
+            Ships = [];
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Title { get; } = "Ship overview";
+
+        public ObservableCollection<ShipOverviewModel> Ships { get; }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
